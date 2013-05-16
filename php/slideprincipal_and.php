dbf3e599f2d516edf29c7f9efcf6c5851ccb2b29
SinstesisApps <webdev_t6@sinstesisdigital.com.mx>
2013-05-16 16:53:26 -0500

    mas

--- php/slideprincipal_and.php
+++ php/slideprincipal_and.php
@@ -11,7 +11,7 @@ include('../../funciones.php');
 			//$select_app="SELECT * FROM app_articulos WHERE posicion='Slide-Principal' AND estatus='1' ORDER BY id DESC";
 			$select_app="SELECT * FROM app_articulos WHERE posicion='Slide-Principal' and estatus='1' 
 							UNION
-						SELECT * FROM app_publicidad where posicion='slide-principal' and dispositivo='ios' and estatus='1'
+						SELECT * FROM app_publicidad where posicion='slide-principal' and dispositivo='android' and estatus='1'
 						ORDER BY orden ASC";
 			
 	$r_app=mysql_query($select_app,$conexion);
